1fd87dee5dae0cb020a3efc6f6e71122e318271b
Gabriel Santana <gabrielsantanasantos798@gmail.com>
2024-05-08 03:36:36 -0300

    Feat: Calling new components

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,28 @@
+import CategoryList from "./_components/category-list";
 import Header from "./_components/header";
+import PromoBanner from "./_components/promo-banner";
+import Recommended from "./_components/recommended";
+import Search from "./_components/search";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <div className="px-5 pt-6">
+        <Search />
+      </div>
+      <div className="px-5 pt-6">
+        <CategoryList />
+      </div>
+      <div className="px-5 pt-6">
+        <PromoBanner src={"/PromoBanner1.svg"} alt="Up to 30% off" />
+      </div>
+      <div className="px-5 pt-6">
+        <Recommended />
+      </div>
+      <div className="px-5 pt-6">
+        <PromoBanner src={"/PromoBanner2.svg"} alt="From R$17.90 on snacks" />
+      </div>
     </>
   );
 }
