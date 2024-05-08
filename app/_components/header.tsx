87b8e06735286cf14b358a29890e269915af3be3
Gabriel Santana <gabrielsantanasantos798@gmail.com>
2024-05-08 01:17:23 -0300

    Fix: Fixed hamburger menu bug

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -8,20 +8,54 @@ import {
 } from "@/app/_components/ui/sheet";
 import { MenuIcon, Sheet } from "lucide-react";
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "./ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogTrigger,
+} from "@radix-ui/react-dialog";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    setIsOpen(true);
+  }, []);
+
+  if (!isOpen) {
+    return null;
+  }
+
+  const handleMenuHamburguer = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div className="flex items-center justify-between p-6">
-      <Image width={100} height={30} src={"/Logo.svg"} alt="Logo" />
-      <Button
-        size="icon"
-        variant="outline"
-        className="border-none bg-transparent"
-      >
-        <MenuIcon />
-      </Button>
+    <div>
+      <div>
+        <Image width={100} height={30} src={"/Logo.svg"} alt="Logo" />
+      </div>
+      <Dialog>
+        <DialogTrigger asChild>
+          {isOpen ? (
+            <Button
+              size="icon"
+              variant="outline"
+              className="border-none bg-transparent"
+              onClick={handleMenuHamburguer}
+            >
+              <MenuIcon />
+            </Button>
+          ) : (
+            <DialogClose>
+              <Button></Button>
+            </DialogClose>
+          )}
+        </DialogTrigger>
+        <DialogContent></DialogContent>
+      </Dialog>
     </div>
   );
 };
