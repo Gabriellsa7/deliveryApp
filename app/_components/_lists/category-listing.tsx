56ea8d373b79ce4a22478f54f0677a2c6f69cb52
Gabriel Santana <gabrielsantanasantos798@gmail.com>
2024-05-08 03:31:58 -0300

    Feat: Adding the category component and listing your products

---
+++ app/_components/_lists/category-listing.tsx
@@ -0,0 +1,35 @@
+import burguerIcon from "@/assets/_category_list-lcons/burguer_icon.svg";
+import dishIcon from "@/assets/_category_list-lcons/dish_icon.svg";
+import iceCreamrIcon from "@/assets/_category_list-lcons/ice_cream.svg";
+import juiceIcon from "@/assets/_category_list-lcons/juice_icon.svg";
+import pizzaIcon from "@/assets/_category_list-lcons/pizza_icon.svg";
+import sushiIcon from "@/assets/_category_list-lcons/sushi_icon.svg";
+
+const types = [
+  {
+    name: "Burguer",
+    icons: burguerIcon,
+  },
+  {
+    name: "Dish",
+    icons: dishIcon,
+  },
+  {
+    name: "Ice cream",
+    icons: iceCreamrIcon,
+  },
+  {
+    name: "Juice",
+    icons: juiceIcon,
+  },
+  {
+    name: "Pizza",
+    icons: pizzaIcon,
+  },
+  {
+    name: "Sushi",
+    icons: sushiIcon,
+  },
+];
+
+export default types;
---
+++ app/_components/category-list.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import types from "./_lists/category-listing";
+// import dishIcons from "@/assets/_category_list_icons/dish.svg";
+
+const CategoryList = () => {
+  return (
+    <div className="grid grid-cols-2 gap-4 ">
+      {types.map(({ name, icons }) => (
+        <div
+          className="flex items-center justify-center gap-3 rounded-full bg-white px-4 py-3 shadow-md"
+          key={name}
+        >
+          <Image src={icons} alt="Logo" width={40} height={40} />
+          <span className="font-bold text-sm">{name}</span>
+        </div>
+      ))}
+      <div></div>
+    </div>
+  );
+};
+
+export default CategoryList;
