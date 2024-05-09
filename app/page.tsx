dc79e53b70807ae7c78901630452b669ac2e7ffc
Gabriel Santana <gabrielsantanasantos798@gmail.com>
2024-05-09 00:48:42 -0300

    Refactor: Add a padding bottom and top in some components

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,17 @@
+import Image from "next/image";
 import CategoryList from "./_components/category-list";
 import Header from "./_components/header";
 import PromoBanner from "./_components/promo-banner";
 import Recommended from "./_components/recommended";
 import RecommendedRestaurants from "./_components/recommended-restaurants";
 import Search from "./_components/search";
+import e from "@/assets/recommended/citrus_shrimp.png";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <div className="px-5 pt-6">
+      <div className="px-5 pt-2">
         <Search />
       </div>
       <div className="px-5 pt-6">
@@ -24,7 +26,7 @@ export default function Home() {
       <div className="px-5 pt-6">
         <PromoBanner src={"/PromoBanner2.svg"} alt="From R$17.90 on snacks" />
       </div>
-      <div className="px-5 pt-6">
+      <div className="px-5 pt-6 pb-9">
         <RecommendedRestaurants />
       </div>
     </>
