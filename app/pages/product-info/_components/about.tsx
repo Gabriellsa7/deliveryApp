58010a1e29fc583bad977ceacd3470fa3854479b
Gabriel Santana <gabrielsantanasantos798@gmail.com>
2024-05-14 01:09:04 -0300

    chorus: create the component about

---
+++ app/pages/product-info/_components/about.tsx
@@ -0,0 +1,15 @@
+const About = () => {
+  return (
+    <div className="flex flex-col gap-3 px-5 py-6">
+      <h1 className="font-bold text-base">About</h1>
+      <span className="font-normal text-sm text-[#7E8392]">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor eos
+        magnam dolores dolorum, consequuntur voluptate velit ut aliquam
+        blanditiis illo earum accusantium odio omnis reiciendis similique!
+        Fugiat earum voluptates ipsam.
+      </span>
+    </div>
+  );
+};
+
+export default About;
