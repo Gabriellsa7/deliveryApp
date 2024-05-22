0cc913cde2d09304c78c017567b3a0be56c77dca
Gabriel Santana <gabrielsantanasantos798@gmail.com>
2024-05-21 23:40:14 -0300

    style: change color font and font size

--- app/pages/product-info/_components/delivery-info.tsx
+++ app/pages/product-info/_components/delivery-info.tsx
@@ -6,17 +6,17 @@ const DeliveryInfo = () => {
     <Card className="mt-6 mx-6 flex justify-around py-3">
       <div className="flex flex-col items-center gap-1">
         <div className="flex items-center gap-2">
-          <span className="font-normal text-sm text-gray-400">delivery</span>
-          <BikeIcon size={18} color="#9ca3af" />
+          <span className="font-normal text-sm text-[#7E8392]">delivery</span>
+          <BikeIcon size={18} color="#7E8392" />
         </div>
-        <span className="font-bold text-base">Free</span>
+        <span className="font-bold text-base text-[#323232]">Free</span>
       </div>
       <div className="flex flex-col items-center gap-1">
         <div className="flex items-center gap-2">
-          <span className="font-normal text-sm text-gray-400">delivery</span>
-          <AlarmClock size={18} color="#9ca3af" />
+          <span className="font-normal text-sm text-[#7E8392]">delivery</span>
+          <AlarmClock size={18} color="#7E8392" />
         </div>
-        <span className="font-bold text-base">40min</span>
+        <span className="font-bold text-base text-[#323232]">40min</span>
       </div>
     </Card>
   );
