5c08fb3979c075e713597a642145d6af8c60aaa8
Gabriel Santana <gabrielsantanasantos798@gmail.com>
2024-05-22 00:02:32 -0300

    feat: create a japanese food list

---
+++ app/_components/_lists/japanese-listing.tsx
@@ -0,0 +1,61 @@
+import japanese0 from "@/assets/japanese-foods/jesse-ballantyne.png";
+import japanese1 from "@/assets/japanese-foods/mahmoud-fawzy.png";
+import japanese2 from "@/assets/japanese-foods/mahmoud-fawzy2.png";
+import japanese3 from "@/assets/japanese-foods/mahmoud-fawzy3.png";
+import japanese4 from "@/assets/japanese-foods/riccardo-bergamini.png";
+import japanese5 from "@/assets/japanese-foods/vaishnav-chogale2.png";
+
+const japaneseFoods = [
+  {
+    id: 1,
+    name: "Jesse Ballantyne",
+    img: japanese0,
+    price: 12,
+    discount: true,
+    discountPercent: 0.2,
+    discountValue: "20%",
+  },
+  {
+    id: 2,
+    name: "Mahmoud Fawzy0",
+    img: japanese1,
+    price: 12,
+    discount: true,
+    discountPercent: 0.2,
+    discountValue: "20%",
+  },
+  {
+    id: 3,
+    name: "Mahmoud Fawzy1",
+    img: japanese2,
+    price: 12,
+    discount: true,
+    discountValue: "20%",
+  },
+  {
+    id: 4,
+    name: "Mahmoud Fawzy2",
+    img: japanese3,
+    price: 12,
+    discount: true,
+    discountValue: "20%",
+  },
+  {
+    id: 5,
+    name: "Riccardo Bergamini",
+    img: japanese4,
+    price: 12,
+    discount: true,
+    discountValue: "20%",
+  },
+  {
+    id: 6,
+    name: "Vaishnav Chogale",
+    img: japanese5,
+    price: 12,
+    discount: true,
+    discountValue: "20%",
+  },
+];
+
+export default japaneseFoods;
