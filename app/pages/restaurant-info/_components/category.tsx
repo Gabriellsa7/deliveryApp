e3bdf9c7e7d58e2828369f57acaa601fc292f4c0
Gabriel Santana <gabrielsantanasantos798@gmail.com>
2024-05-21 23:41:35 -0300

    feat: Create category component to restaurant page info

---
+++ app/pages/restaurant-info/_components/category.tsx
@@ -0,0 +1,14 @@
+const Category = () => {
+  return (
+    <div className="flex items-center justify-around mt-6 mx-6 ">
+      <div className="flex justify-center bg-[#F4F4F5] py-1 px-2 rounded-lg min-w-[167px] items-center">
+        <span className="text-[#7E8392] text-base font-normal">Japanese</span>
+      </div>
+      <div className="flex justify-center bg-[#F4F4F5] py-1 px-2 rounded-lg min-w-[167px] items-center">
+        <span className="text-[#7E8392] text-base font-normal">Juices</span>
+      </div>
+    </div>
+  );
+};
+
+export default Category;
