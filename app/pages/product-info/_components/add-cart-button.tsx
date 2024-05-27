4d75c12dfea329aae523726fd7f0ef1c8e6884fb
Gabriel Santana <gabrielsantanasantos798@gmail.com>
2024-05-27 00:18:10 -0300

    Feat: create the logic to add product in the database

--- app/pages/product-info/_components/add-cart-button.tsx
+++ app/pages/product-info/_components/add-cart-button.tsx
@@ -1,9 +1,54 @@
+"use client";
 import { Button } from "@/app/_components/ui/button";
+import { Product } from "@/app/context/cart-context";
+import { useState } from "react";
 
-const AddCartButton = () => {
+const AddCartButton = ({ product }: { product: Product }) => {
+  const [cart, setCart] = useState<Product[]>([]);
+
+  const addToCart = async (product: Product) => {
+    const existingProductIndex = cart.findIndex(
+      (item) => item.id === product.id
+    );
+    let updatedCart;
+
+    const productQuantity = product.quantity ?? 1;
+
+    if (existingProductIndex >= 0) {
+      updatedCart = [...cart];
+      updatedCart[existingProductIndex].quantity =
+        (updatedCart[existingProductIndex].quantity ?? 0) + productQuantity;
+    } else {
+      updatedCart = [...cart, { ...product, quantity: productQuantity }];
+    }
+
+    try {
+      const response = await fetch("/api/products-cart", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(product),
+      });
+
+      if (response.ok) {
+        const updatedCartFromServer = await response.json();
+        setCart(updatedCartFromServer);
+      } else {
+        console.error("Failed to add to cart");
+      }
+    } catch (error) {
+      console.error("Error adding to cart:", error);
+    }
+  };
   return (
     <div className="flex items-center w-full justify-center mt-6 mb-3">
-      <Button className="bg-[#EA1D2C] w-[90%]">Add to cart</Button>
+      <Button
+        onClick={() => addToCart(product)}
+        className="bg-[#EA1D2C] w-[90%]"
+      >
+        Add to cart
+      </Button>
     </div>
   );
 };
