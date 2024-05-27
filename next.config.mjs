3b219ebd2dd5d50cc9fe500b09b07aed4ce04fec
Gabriel Santana <gabrielsantanasantos798@gmail.com>
2024-05-27 00:17:50 -0300

    Feat: Add proxy to trying change the address with back

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:8888/api/:path*", // Proxy para o backend
+      },
+    ];
+  },
+};
 
 export default nextConfig;
