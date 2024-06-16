0dc115e500c80bb93f8367a56ec32a6060a72d3d
Gabriel Santana <gabrielsantanasantos798@gmail.com>
2024-06-16 00:36:07 -0300

    fix: fix some errors

--- app/pages/favorite-restaurants/page.tsx
+++ app/pages/favorite-restaurants/page.tsx
@@ -1,9 +1,30 @@
+"use client";
 import restaurants from "@/app/_components/_lists/recommended-restaurant-listing";
 import Header from "@/app/_components/header";
+import {
+  FavoritesProvider,
+  useFavorites,
+} from "@/app/context/favorite-context";
 import { AlarmClock, BikeIcon } from "lucide-react";
 import Image from "next/image";
+import { MdDeleteForever } from "react-icons/md";
 
-const FavoriteRestaurants = () => {
+export function FavoriteRestaurants() {
+  return (
+    <FavoritesProvider>
+      {" "}
+      {/* Wraps the "My Favorites" page with the FavoritesProvider provider */}
+      <FavoriteRestaurantsContent />
+    </FavoritesProvider>
+  );
+}
+
+const FavoriteRestaurantsContent = () => {
+  const { favorites, removeFavorites } = useFavorites();
+
+  const handleDeleteFavorites = (recipeId: any) => {
+    removeFavorites(recipeId);
+  };
   return (
     <>
       <Header />
@@ -11,33 +32,50 @@ const FavoriteRestaurants = () => {
         <h1 className="font-bold text-lg text-[#323232]">
           Favorite Restaurants
         </h1>
-        <div className="flex gap-3 flex-col">
-          <Image
-            src={restaurants[0].img}
-            alt="Favorite"
-            width={350}
-            className="rounded-xl max-h-[138px]"
-          />
-          <div>
-            <span className="font-bold text-sm text-[#323232]">
-              {restaurants[0].name}
-            </span>
-            <div className="flex items-center gap-2">
-              <div className="flex items-center gap-1">
-                <div className="flex items-center gap-2">
-                  <BikeIcon size={12} color="#EA1D2C" />
+        {favorites &&
+          favorites.map((favorite) => (
+            <div key={favorite.id} className="flex gap-3 flex-col">
+              <div className="relative">
+                <Image
+                  src={favorite.img}
+                  alt="Favorite"
+                  width={350}
+                  className="rounded-xl max-h-[138px]"
+                />
+                <div className="absolute top-2 right-2">
+                  <button onClick={() => handleDeleteFavorites(favorite.id)}>
+                    <MdDeleteForever
+                      className="text-[#3e404a] hover:text-slate-400 duration-500"
+                      size={32}
+                    />
+                  </button>
                 </div>
-                <span className="font-bold text-xs text-[#7E8392]">Free</span>
               </div>
-              <div className="flex  items-center gap-1">
+              <div>
+                <span className="font-bold text-sm text-[#323232]">
+                  {favorite.name}
+                </span>
                 <div className="flex items-center gap-2">
-                  <AlarmClock size={12} color="#EA1D2C" />
+                  <div className="flex items-center gap-1">
+                    <div className="flex items-center gap-2">
+                      <BikeIcon size={12} color="#EA1D2C" />
+                    </div>
+                    <span className="font-bold text-xs text-[#7E8392]">
+                      Free
+                    </span>
+                  </div>
+                  <div className="flex  items-center gap-1">
+                    <div className="flex items-center gap-2">
+                      <AlarmClock size={12} color="#EA1D2C" />
+                    </div>
+                    <span className="font-bold text-xs text-[#7E8392]">
+                      40min
+                    </span>
+                  </div>
                 </div>
-                <span className="font-bold text-xs text-[#7E8392]">40min</span>
               </div>
             </div>
-          </div>
-        </div>
+          ))}
       </div>
     </>
   );
