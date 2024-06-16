dd1883afd34f87d92c9d4814ba83b5b148e60f97
Gabriel Santana <gabrielsantanasantos798@gmail.com>
2024-06-16 00:33:01 -0300

    chorus: add FavoritesProvider to main page

--- app/page.tsx
+++ app/page.tsx
@@ -6,29 +6,32 @@ import Recommended from "./_components/recommended";
 import RecommendedRestaurants from "./_components/recommended-restaurants";
 import Search from "./_components/search";
 import { CartProvider } from "./context/cart-context";
+import { FavoritesProvider } from "./context/favorite-context";
 
 export default function Home() {
   return (
-    <CartProvider>
-      <Header />
-      <div className="px-5 pt-2">
-        <Search />
-      </div>
-      <div className="px-5 pt-6">
-        <CategoryList />
-      </div>
-      <div className="px-5 pt-6">
-        <PromoBanner src={"/PromoBanner1.svg"} alt="Up to 30% off" />
-      </div>
-      <div className="px-5 pt-6">
-        <Recommended />
-      </div>
-      <div className="px-5 pt-6">
-        <PromoBanner src={"/PromoBanner2.svg"} alt="From R$17.90 on snacks" />
-      </div>
-      <div className="px-5 pt-6 pb-9">
-        <RecommendedRestaurants />
-      </div>
-    </CartProvider>
+    <FavoritesProvider>
+      <CartProvider>
+        <Header />
+        <div className="px-5 pt-2">
+          <Search />
+        </div>
+        <div className="px-5 pt-6">
+          <CategoryList />
+        </div>
+        <div className="px-5 pt-6">
+          <PromoBanner src={"/PromoBanner1.svg"} alt="Up to 30% off" />
+        </div>
+        <div className="px-5 pt-6">
+          <Recommended />
+        </div>
+        <div className="px-5 pt-6">
+          <PromoBanner src={"/PromoBanner2.svg"} alt="From R$17.90 on snacks" />
+        </div>
+        <div className="px-5 pt-6 pb-9">
+          <RecommendedRestaurants />
+        </div>
+      </CartProvider>
+    </FavoritesProvider>
   );
 }
