58a9bff02b99df2e9f9efbf23ccf6e9dc80d3e71
Gabriel Santana <gabrielsantanasantos798@gmail.com>
2024-06-16 23:30:22 -0300

    feat: created burger listing

---
+++ app/_components/_lists/burgers-listing.tsx
@@ -0,0 +1,65 @@
+import burger1 from "@/assets/burgers/amirali-mirhashemian-88YAXjnpvrM-unsplash.png";
+import burger2 from "@/assets/burgers/amirali-mirhashemian-9Bqiusimq6M-unsplash (1).png";
+import burger3 from "@/assets/burgers/amirali-mirhashemian-d-yAWqj-DRg-unsplash.png";
+import burger4 from "@/assets/burgers/amirali-mirhashemian-jh5XyK4Rr3Y-unsplash (1).png";
+import burger5 from "@/assets/burgers/amirali-mirhashemian-sc5sTPMrVfk-unsplash (1).png";
+import burger6 from "@/assets/burgers/amirali-mirhashemian-uhgJYtrO3fY-unsplash 1.png";
+
+const burgers = [
+  {
+    id: 1,
+    name: "Beef Burger",
+    img: burger1,
+    price: 12,
+    discount: true,
+    discountPercent: 0.2,
+    discountValue: "20%",
+  },
+  {
+    id: 2,
+    name: "Bacon Burger",
+    img: burger2,
+    price: 12,
+    discount: true,
+    discountPercent: 0.2,
+    discountValue: "20%",
+  },
+  {
+    id: 3,
+    name: "Cheese Burger",
+    img: burger3,
+    price: 12,
+    discount: true,
+    discountPercent: 0.2,
+    discountValue: "20%",
+  },
+  {
+    id: 4,
+    name: "multi-layered cheeseburger",
+    img: burger4,
+    price: 12,
+    discount: true,
+    discountPercent: 0.2,
+    discountValue: "20%",
+  },
+  {
+    id: 5,
+    name: "Limani Burger",
+    img: burger5,
+    price: 12,
+    discount: true,
+    discountPercent: 0.2,
+    discountValue: "20%",
+  },
+  {
+    id: 6,
+    name: "Eden Burger",
+    img: burger6,
+    price: 12,
+    discount: true,
+    discountPercent: 0.2,
+    discountValue: "20%",
+  },
+];
+
+export default burgers;
