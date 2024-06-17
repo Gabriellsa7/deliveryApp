8a944e230ba2a1e9117f8dc67bae829d6a518b81
Gabriel Santana <gabrielsantanasantos798@gmail.com>
2024-06-16 23:30:02 -0300

    chorus: add url system

--- app/_components/category-list.tsx
+++ app/_components/category-list.tsx
@@ -1,19 +1,31 @@
 import Image from "next/image";
 import types from "./_lists/category-listing";
-// import dishIcons from "@/assets/_category_list_icons/dish.svg";
+import Link from "next/link";
 
 const CategoryList = () => {
   return (
     <div className="grid grid-cols-2 gap-4 ">
-      {types.map(({ name, icons, id }) => (
-        <div
-          className="flex items-center justify-center gap-3 rounded-full bg-white px-4 py-3 shadow-md"
-          key={id}
-        >
-          <Image src={icons} alt="Logo" width={40} height={40} />
-          <span className="font-bold text-sm">{name}</span>
-        </div>
-      ))}
+      {types.map(({ name, icons, id, url }) =>
+        url ? (
+          <Link href={url} key={id}>
+            <div
+              className="flex items-center justify-center gap-3 rounded-full bg-white px-4 py-3 shadow-md"
+              key={id}
+            >
+              <Image src={icons} alt={`${name} icon`} width={40} height={40} />
+              <span className="font-bold text-sm">{name}</span>
+            </div>
+          </Link>
+        ) : (
+          <div
+            className="flex items-center justify-center gap-3 rounded-full bg-white px-4 py-3 shadow-md"
+            key={id}
+          >
+            <Image src={icons} alt={`${name} icon`} width={40} height={40} />
+            <span className="font-bold text-sm">{name}</span>
+          </div>
+        )
+      )}
       <div></div>
     </div>
   );
