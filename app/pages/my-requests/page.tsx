e302ca537eba3cedb88023002179b131bef1bc2d
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-07-05 01:07:15 -0300

    Created: MyRequests Page

--- app/pages/my-requests/page.tsx
+++ app/pages/my-requests/page.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import Header from "@/app/_components/header";
+import React from "react";
+import Cards from "./components/cards";
 
 const MyRequests = () => {
   return (
-    <div>MyRequests</div>
-  )
-}
+    <main className="px-6 pt-2 flex flex-col gap-6">
+      <Header />
+      <span>My Requests</span>
+      <section className="flex flex-col gap-3">
+        <Cards />
+        <Cards />
+      </section>
+    </main>
+  );
+};
 
 export default MyRequests;
