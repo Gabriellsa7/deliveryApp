44dd440686d47e4854cbe7a6b44b60a1679e7888
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-07-06 02:37:51 -0300

    chorus: added more foods

--- app/_components/_lists/general-foods-list.tsx
+++ app/_components/_lists/general-foods-list.tsx
@@ -19,6 +19,13 @@ import carrotJuice from "@/assets/juices/carrot-juice.png";
 import pineappleJuice from "@/assets/juices/pineapple-juice.png";
 import orangeJuice from "@/assets/juices/orange-juice.png";
 
+import chickenInSauce from "@/assets/recommended/new_chicken_in_sauce.jpeg";
+import citrusShrimp from "@/assets/recommended/citrus-shrimp.jpeg";
+import classicRamen from "@/assets/recommended/classic-ramen.jpeg";
+import greenTempura from "@/assets/recommended/green-tempura.jpeg";
+import hotRollCombo from "@/assets/recommended/hot-holl-combo.webp";
+import supremePicanha from "@/assets/recommended/supreme-picanha.jpeg";
+
 const generalFoods = [
   {
     id: 1,
@@ -181,6 +188,72 @@ const generalFoods = [
     discount: true,
     discountPercent: 0.2,
     discountValue: "20%",
+  },
+  {
+    id: 19,
+    name: "Chicken in Sauce",
+    img: chickenInSauce,
+    price: 20.0,
+    restaurantName: "Bistro Verdejante",
+    discount: true,
+    discountPercent: 0.2,
+    discountValue: "20%",
+    priceWithDiscount: 0,
+  },
+  {
+    id: 20,
+    name: "Citrus Shrimp",
+    img: citrusShrimp,
+    price: 20.0,
+    restaurantName: "Bistro Verdejante",
+    discount: true,
+    discountPercent: 0.2,
+    discountValue: "20%",
+    priceWithDiscount: 0,
+  },
+  {
+    id: 21,
+    name: "Classic Ramen",
+    img: classicRamen,
+    price: 20.0,
+    restaurantName: "Bistro Verdejante",
+    discount: true,
+    discountPercent: 0.2,
+    discountValue: "20%",
+    priceWithDiscount: 0,
+  },
+  {
+    id: 22,
+    name: "Green Tempura",
+    img: greenTempura,
+    price: 20.0,
+    restaurantName: "Bistro Verdejante",
+    discount: true,
+    discountPercent: 0.2,
+    discountValue: "20%",
+    priceWithDiscount: 0,
+  },
+  {
+    id: 23,
+    name: "Hot Roll Combo",
+    img: hotRollCombo,
+    price: 20.0,
+    restaurantName: "Bistro Verdejante",
+    discount: true,
+    discountPercent: 0.2,
+    discountValue: "20%",
+    priceWithDiscount: 0,
+  },
+  {
+    id: 24,
+    name: "Supreme Picanha",
+    img: supremePicanha,
+    price: 20.0,
+    restaurantName: "Bistro Verdejante",
+    discount: true,
+    discountPercent: 0.2,
+    discountValue: "20%",
+    priceWithDiscount: 0,
   },
 ];
 
